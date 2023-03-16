2c02b267cf7cd8d2294c689c346dd9fde117161f
SanzharKadyrkulov <kadyrkulov.980@gmail.com>
2023-03-16 19:22:33 +0600

    laskj

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import MainLayout from "../layouts/MainLayout";
+import ProductsPage from "../pages/ProductsPage";
 
 const MainRoutes = () => {
 	return (
 		<Routes>
 			<Route element={<MainLayout />}>
 				<Route path="/" element={<HomePage />} />
+				<Route path="/products" element={<ProductsPage />} />
 			</Route>
 		</Routes>
 	);

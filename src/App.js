3e0739c2bbcefa84fa53e7d288f7f58035c61012
SanzharKadyrkulov <kadyrkulov.980@gmail.com>
2023-03-17 18:32:39 +0600

    db.json

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MainRoutes from "./routes/MainRoutes";
 import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+
+const API = "http://localhost:8000/computers";
 
 function App() {
+	const [products, setProducts] = useState([]);
+
+	async function getProducts() {
+		// const res = await fetch(API);
+		// const data = await res.json();
+		// console.log(data);
+		const { data } = await axios.get(API);
+		setProducts(data);
+	}
+	console.log(products);
+	useEffect(() => {
+		getProducts();
+	}, []);
+
 	return <MainRoutes />;
 }
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function HomePage() {
-	return <div>HomePage</div>;
+	const [isBlue, setIsBlue] = useState(false);
+
+	// useEffect(() => {
+	// 	console.log("First mount");
+	// }, []);
+
+	// useEffect(() => {
+	// 	console.log("update");
+	// }, [isBlue]);
+
+	// useEffect(() => {
+	// 	return () => {
+	// 		console.log("death");
+	// 	};
+	// }, []);
+
+	// function move(e) {
+	// 	console.log(e.clientX, e.clientY);
+	// }
+	// useEffect(() => {
+	// 	window.addEventListener("mousemove", move);
+	// 	return () => {
+	// 		window.removeEventListener("mousemove", move);
+	// 	};
+	// }, []);
+
+	return (
+		<div>
+			<div
+				style={{
+					width: "100px",
+					height: "100px",
+					background: isBlue ? "blue" : "red",
+				}}
+			></div>
+			<button onClick={() => setIsBlue(!isBlue)}>change</button>
+		</div>
+	);
 }
 
 export default HomePage;
